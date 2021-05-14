04c139104e3491c25587e04791edc2f694cb7e91
RocioLetona <75388342+RocioLetona@users.noreply.github.com>
2021-05-14 12:57:14 +0200

    Create ComercialVO.cs

---
+++ Dashboard/GestionDatos/ComercialVO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionDatos
+{
+    public class ComercialVO
+    {
+        private int numero;
+
+        public ComercialVO(int numero)
+        {
+            this.Numero = numero;
+        }
+
+        public int Numero { get => numero; set => numero = value; }
+    }
+}
